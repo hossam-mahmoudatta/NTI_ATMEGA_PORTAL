27f0cf28acef0da87925c3a727094a3e279c50e3
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-24 21:36:59 +0300

    Adding some files to Task05

---
+++ TASK05_0-99/APP/APP_PROGRAM.c
@@ -0,0 +1,40 @@
+/******************************************************************************
+ *
+ * Application
+ *
+ * File Name: TASK3_main.c
+ *
+ * Description: Application file for testing the gpio driver
+ *
+ * Author: Hossam Mahmoud
+ *
+ *******************************************************************************/
+
+
+/*******************************************************************************
+ *                              					 Application Libraries                      					  *
+ *******************************************************************************/
+
+#include "APP_INTERFACE.h"
+
+/*******************************************************************************
+ *                              					 Application Libraries                      					  *
+ *******************************************************************************/
+
+extern u16 ADC_Result = 0;
+
+void systemInit(void) {
+	LCD_voidInit();
+	ADC_voidInit();
+	GPIO_voidSetPinDirection(PORT_A, PIN_1, PIN_INPUT);
+
+	LCD_voidDisplayString("Resistor Value: ", 0, 0);
+}
+
+void readResistorADC(void) {
+	ADC_Result = ADC_voidStartConversionPolling(CHANNEL_1);
+	LCD_voidIntgerToString(ADC_Result, 1, 0);
+	_delay_ms(250);
+
+}
+
