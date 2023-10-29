5bf851cfbd70a7c32781fcbbcd238d44becb3fcf
Hossam <hossam.uddin1@gmail.com>
2023-10-29 08:05:08 +0200

    added some changes to the adjustServo function

--- TASK28_TIMER1-SERVO/APP/APP_PROGRAM.c
+++ TASK28_TIMER1-SERVO/APP/APP_PROGRAM.c
@@ -23,23 +23,31 @@
 
 u16 ADC_Result = 0;
 u8 Mapping = 0;
+u16 angleValue = 0;
 
 void SYSTEM_INITIALIZATION(void) {
 	// Initializing LCD Module
 	LCD_voidInit();
 	LCD_voidSetCursor(0, 0);
 	LCD_voidDisplayString("Servo Motor");
+	LCD_voidSetCursor(1, 0);
+	LCD_voidDisplayString("Angle: ");
 	GPIO_voidSetPinDirection(PORT_D, PIN_5, PIN_OUTPUT);
 
 	ADC_voidInit();
 
 	// Initializing Timer0 Module
 	TIMER1_Initialization();
+	TIMER1A_SetTop_FASTPWM();
 
 }
 
 void adjustServo(void){
 	ADC_Result = ADC_voidStartConversionPolling();
+	angleValue = mapServo(ADC_Result);
+	TIMER1A_SetCOMPAREMATCH_FASTPWM(angleValue);
+	LCD_voidSetCursor(1, 7);
+	LCD_voidDisplayString("Angle: ");
 
 }
 
